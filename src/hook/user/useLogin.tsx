ad7ccb57e71a61a234ec0d2406c54b11139a4c18
GOOMBANGEE <goom@goomba.org>
2024-03-05 17:14:14 +0900

    ADD User Login Page, Hook

---
+++ src/hook/user/useLogin.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { setCookie } from "../../Cookie.tsx";
+import { useGlobalStore } from "../../store/GlobalStore.tsx";
+import { useTokenStore } from "../../store/TokenStore.tsx";
+import { useEnvStore } from "../../store/EnvStore.tsx";
+import { handleAxiosError } from "../handleAxiosError.tsx";
+import { useUserStore } from "../../store/UserStore.tsx";
+
+export default function useLogin() {
+  const { globalState, setGlobalState } = useGlobalStore();
+  const { envState } = useEnvStore();
+  const { tokenState, setTokenState, getAccessToken } = useTokenStore();
+  const { userState } = useUserStore();
+
+  const login = async () => {
+    try {
+      const response = await axios.post(`${envState.userUrl}/login`, {
+        email: userState.email,
+        password: userState.password,
+      });
+
+      setTokenState({
+        ...tokenState,
+        accessToken: response.data.access_token,
+      });
+
+      const today = new Date();
+      const expireDate = today.setDate(today.getDate() + 1);
+      setCookie("refresh_token", response.data.refresh_token, {
+        sameSite: "strict",
+        path: "/",
+        expires: new Date(expireDate),
+      });
+      getAccessToken(tokenState.accessToken);
+      return true;
+    } catch (error) {
+      handleAxiosError(globalState, setGlobalState, error);
+      return false;
+    }
+  };
+
+  return { login };
+}
---
+++ src/page/user/Login.tsx
@@ -0,0 +1,123 @@
+import { FormEvent, useState } from "react";
+import { useGlobalStore } from "../../store/GlobalStore.tsx";
+import Modal from "../../component/Modal.tsx";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "../../store/UserStore.tsx";
+import useValidateUser from "../../hook/user/useValidateUser.tsx";
+import useRenderErrorMessage from "../../hook/user/useRenderErrorMessage.tsx";
+import useLogin from "../../hook/user/useLogin.tsx";
+
+export default function Login() {
+  const { globalState } = useGlobalStore();
+  const { userState, setUserState } = useUserStore();
+
+  const [validateUser, setValidateUser] = useState<ValidateUser>({
+    emailError: "",
+    nicknameError: "",
+    passwordError: "",
+  });
+
+  const { isEmailValid, isPasswordValid } = useValidateUser();
+  const { login } = useLogin();
+  const navigate = useNavigate();
+  const [loginFail, setLoginFail] = useState(false);
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (
+      !isEmailValid({ value: userState.email, validateUser, setValidateUser })
+    ) {
+      return;
+    }
+    if (
+      !isPasswordValid({
+        value: userState.password,
+        validateUser,
+        setValidateUser,
+      })
+    ) {
+      return;
+    }
+
+    if (await login()) {
+      navigate("/");
+      return;
+    }
+    setLoginFail(true);
+  };
+  return (
+    <div>
+      <div className="mx-auto pt-60 text-center md:p-16 dark:text-slate-200">
+        <div className="w-full rounded-lg shadow-md">
+          <div className="p-4">Community</div>
+          <div className="mx-auto flex w-5/6 flex-col">
+            <div className="my-6 font-semibold">로그인</div>
+
+            <form className="flex w-full flex-col" onSubmit={handleLogin}>
+              <div className="mb-1 text-start">이메일</div>
+              <input
+                placeholder="이메일 입력"
+                className="mx-auto mb-2 w-full border-2 border-gray-500 bg-black p-2 focus:bg-indigo-100 focus:text-black focus:opacity-90"
+                onChange={(e) => {
+                  setUserState({
+                    ...userState,
+                    email: e.target.value,
+                  });
+                  setValidateUser({
+                    ...validateUser,
+                    emailError: "",
+                  });
+                  setLoginFail(false);
+                }}
+              />
+              <div className="mb-1 text-start">비밀번호</div>
+              <input
+                type="password"
+                placeholder="비밀번호 입력"
+                className="mx-auto mb-2 w-full border-2 border-gray-500 bg-black p-2 focus:bg-indigo-100 focus:text-black focus:opacity-90"
+                onChange={(e) => {
+                  setUserState({
+                    ...userState,
+                    password: e.target.value,
+                  });
+                  setValidateUser({
+                    ...validateUser,
+                    passwordError: "",
+                  });
+                  setLoginFail(false);
+                }}
+              />
+              <button
+                type="submit"
+                className="w- mx-auto mb-2 mt-4 w-full bg-indigo-600 p-2 font-semibold text-white"
+              >
+                로그인
+              </button>
+              {useRenderErrorMessage(validateUser.emailError)}
+              {useRenderErrorMessage(validateUser.passwordError)}
+              {loginFail ? (
+                <div className="mx-auto text-start text-base text-red-500">
+                  이메일 또는 비밀번호를 확인해 주세요
+                </div>
+              ) : null}
+            </form>
+
+            <div className="mx-auto flex ">
+              <a href="/user/helppw" className="my-4 text-blue-400">
+                로그인할 수 없습니까?
+              </a>
+              <div className="mx-2 my-4">|</div>
+              <a href="/user/register" className="my-4 text-blue-400">
+                계정 만들기
+              </a>
+            </div>
+            <div className="my-4">또는 다음을 사용하여 계속하기</div>
+            <div>oauth list</div>
+            {globalState.modalMessage ? <Modal /> : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
